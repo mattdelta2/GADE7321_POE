9dd4d830e23ed2388efaeddfbe195259788bbb6b
Matthew Wierenga <matt.wierenga@outlook.com>
2023-06-28 14:58:05 +0200

    Restarting

--- GADE7321_POE_Part2-main/Elemental Battlegrounds/Assets/Scripts/Game Scene/ElementSelector.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ElementSelector : MonoBehaviour
-{
-    //Implementation of Selection Elements during the fight, without changing the Health value of the players
-
-}

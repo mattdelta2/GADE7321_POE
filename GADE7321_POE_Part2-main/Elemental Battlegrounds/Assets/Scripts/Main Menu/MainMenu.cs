c9882d18c60c662982fab30aec435fbfceeaa659
Matthew Wierenga <matt.wierenga@outlook.com>
2023-06-28 10:47:38 +0200

    Added part 2 to part 3

---
+++ GADE7321_POE_Part2-main/Elemental Battlegrounds/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Options()
+    {
+        SceneManager.LoadScene("HowToPlay");
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SingleOrMulti");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
